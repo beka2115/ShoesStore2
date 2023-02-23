c995e6818f7b1212b738f318dd464250a6e9694b
beka2115 <dinispro2112@mail.ru>
2023-02-23 19:05:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/shoesstore/ProfileFragment.kt
@@ -0,0 +1,54 @@
+package com.example.shoesstore
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.shoesstore.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        with(binding){
+            cardNotifications.textForCard.text = "Уведомление"
+            cardMyData.textForCard.text = "Мои Данные"
+            cardCards.textForCard.text = "Карты"
+            cardHelp.textForCard.text ="Помощь"
+            cardQuestion.textForCard.text = "Вопросы и ответы"
+            cardNotifications.imgNext.visibility = View.GONE
+            cardNotifications.cardContainer.isClickable = false
+            cardNotifications.switchNotifications.visibility = View.VISIBLE
+            Glide.with(cardMyOrders.imgForCard).load(R.drawable.my_orders).into(cardMyOrders.imgForCard)
+            Glide.with(cardNotifications.imgForCard).load(R.drawable.notification).into(cardNotifications.imgForCard)
+            Glide.with(cardCards.imgForCard).load(R.drawable.cards).into(cardCards.imgForCard)
+            Glide.with(cardHelp.imgForCard).load(R.drawable.help_24).into(cardHelp.imgForCard)
+            Glide.with(cardQuestion.imgForCard).load(R.drawable.question_24).into(cardQuestion.imgForCard)
+        }
+    }
+
+    private fun initListeners(){
+        with(binding){
+            
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shoesstore/ProfileViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.shoesstore
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel:ViewModel() {
+}
